6f704fac6a07cdd9f34c00c3f17fa362cde37983
rohit1604jha@gmail.com <rohit2004jha@gmail.com>
2025-09-27 11:12:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="itinearary planer",
+    version="0.1.0",
+    author="Rohit",
+    author_email="rohit1604jha@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
